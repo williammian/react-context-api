0910050fc583f07c8cd62fb8dc7c013eb3b170c3
William Mian <william_mian@yahoo.com.br>
2024-09-27 16:04:25 -0300

    explorando a Context API

--- src/context/CarrinhoContext.jsx
+++ src/context/CarrinhoContext.jsx
@@ -1,10 +1,12 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const CarrinhoContext = createContext();
 
 export const CarrinhoProvider = ({children}) => {
+  const [carrinho, setCarrinho] = useState([]);
+
   return (
-    <CarrinhoContext.Provider>
+    <CarrinhoContext.Provider value={{ carrinho, setCarrinho }}>
       {children}
     </CarrinhoContext.Provider>
   )
